450c13eb7c44386d14053d0e708926a2639e2a91
amcnea <amcnea@users.noreply.github.com>
2018-12-02 18:14:27 -0600

    Removed trailing return from stab_jokes.php

--- scripts/stab_jokes.php
+++ scripts/stab_jokes.php
@@ -4,4 +4,4 @@ require __DIR__ . "/../lib/QuoteServer.php";
 
 echo (new QuoteServer())->
     loadFromUrl('https://raw.githubusercontent.com/stabbstream/twitch/master/quote_lists/stabb.txt')->
-    pickRandomQuote() . "\n";
+    pickRandomQuote();
